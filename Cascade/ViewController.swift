4084a902c72e1fc7a005f84eeaea4370f58423fe
Dmitrii Ivanov <dmitrii@getqardio.com>
2017-05-23 12:54:15 +0300

    project init

---
+++ Cascade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cascade
+//
+//  Created by Dmitrii on 23/05/2017.
+//  Copyright © 2017 GridCascade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

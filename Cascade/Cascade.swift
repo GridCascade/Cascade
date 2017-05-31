e4c8c6846ed821a52a16e5736ed4a3c52a1e7c67
Dmitrii Ivanov <topolog@icloud.com>
2017-05-31 11:10:48 +0200

    Create architectural wireframe #11 - pch removed

--- Cascade/Cascade.swift
+++ Cascade/Cascade.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 GridCascade. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /**
  Root class to use from the client code
--- Cascade/UI/UI.swift
+++ Cascade/UI/UI.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 GridCascade. All rights reserved.
 //
 
-import Foundation
+import PureLayout
 
 protocol UIProtocol: class {
     func view() -> UIView
